2b586409c5086cff450975b2e7c28fdc4debe7d8
KoffTheMinister <nachokoffler@gmail.com>
2025-04-30 15:45:06 -0300

    cambie la cosa de corrida y lo de tirada que estaba mezclado

--- tp2.py
+++ tp2.py
@@ -8,8 +8,8 @@ if sys.argv[1]!="-c" or sys.argv[3]!="-n" or sys.argv[5]!="-s" or sys.argv[7]!="
     print(f"Uso: Python SimulacionRuleta.py -c <Cantidad de tiradas> -n <numero de corridas> -s <estrategia ({estrategias})> -a <capital a usar (infinito (i) - numero de capital)>")
     sys.exit(1)
 
-cantTiradas=int(sys.argv[2])
-cantCorridas=int(sys.argv[4])
+cantTiradas=int(sys.argv[4])
+cantCorridas=int(sys.argv[2])
 if(sys.argv[8] == 'i'):
     capital = 'i'
 else:
