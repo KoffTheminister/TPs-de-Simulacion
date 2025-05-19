19a18a5a83d37d2b7f557f4eec62acebb0e11c24
KoffTheMinister <nachokoffler@gmail.com>
2025-05-19 17:34:09 -0300

    todos los generadores hechos

--- tp22.py
+++ tp22.py
@@ -1,20 +1,70 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+import math
+from scipy.stats import chi2
+from scipy.stats import norm
 
-def generador2(seed, n):
-  lista = np.array([])
-  a = 2**16 + 3
-  m = 2**31
+def gcl(seed, n):
+  lista = []#lista = np.array([])
+  a = 1664525#2**16 + 3
+  m = 2**32
   ultimo = (a*seed)%m
-  lista = np.append(lista, ultimo/m)
+  lista.append(ultimo/m) #lista = np.append(lista, ultimo/m)
   i = 1
   while(i <= n):
     ultimo = (a*ultimo)%m
-    lista = np.append(lista, ultimo/m)
+    lista.append(ultimo/m)#lista = np.append(lista, ultimo/m)
     i += 1
 
   return lista
+
+def normal_gen1(media, ds, k, n): #basico
+  lista = gcl(1236543, k*n)
+  nuevaLista = []
+  for i in range(n):
+    suma = np.sum(lista[k*i:k*(i+1)])
+    nuevaLista.append(ds*((12/k)**1/2)*(suma - k/2) + media)
+  return nuevaLista
+
+def normal_gen2(media, ds, k, n): #directo
+  lista = gcl(123654, k*n)
+  nuevaLista = []
+  for i in range(1, n, 2):
+    nuevaLista.append(np.sqrt(-2*math.log(lista[i]))*math.cos(2*math.pi*lista[i+1]))
+    nuevaLista.append(np.sqrt(-2*math.log(lista[i]))*math.sin(2*math.pi*lista[i+1]))
+  return nuevaLista
+
+def poisson_gen(myLambda, m):
+  lista = gcl(1236543, m)
+  expo = math.exp(-myLambda)
+  pList = []
+  for n in lista:
+    sum_prob = 0
+    i = 0
+    while True:
+      term = expo * (myLambda ** i) / math.factorial(i)
+      sum_prob += term
+      if n < sum_prob:
+        pList.append(i)
+        break
+      i += 1
+  return pList
+
+def pascal_gen(k, p, m):
+  lista = gcl(1236543, m)
+  pList = []
+  for n in lista:
+    sum_prob = 0
+    i = 0
+    while True:
+      term = math.comb(k + i - 1, i)*(p**k)*((1 - p)**i)
+      sum_prob += term
+      if n < sum_prob:
+        pList.append(i)
+        break
+      i += 1
+  return pList
 
 def run_test(numbers):
   mean = np.mean(numbers)
@@ -80,13 +130,24 @@ def chi_cuadrado(data, k, alpha):
 
 
 def calculateAreaUnderUni(x_min, x_max, x):
-    return (1 / (x_max - x_min)) * (x - x_min)
+  return (1 / (x_max - x_min)) * (x - x_min)
 
-def kolmogorovSmirnovTest(samples, n_divs, x_min, x_max):
+def calculateAreaUnderPoi(x, myLambda):
+  expo = math.exp(-myLambda)
+  for i in range(x + 1):
+    suma += (myLambda**i)/math.factorial(i)
+  return expo*suma
+
+def calculateAreaUnderPas(k, p, x):
+  for i in range(x + 1):
+    suma += math.comb(k + i - 1, i)*(p**k)*((1 - p)**i)
+  return suma
+
+def kolmogorovSmirnovTest(samples, n_divs):
   samples = np.sort(samples)
   histo = np.zeros(n_divs)
-  # x_min = min(samples)
-  # x_max = max(samples)
+  x_min = min(samples)
+  x_max = max(samples)
   divSize = (x_max - x_min) / n_divs
   for s in samples:
     index = int((s - x_min) / divSize)
@@ -106,11 +167,11 @@ def kolmogorovSmirnovTest(samples, n_divs, x_min, x_max):
           maxDiff = diff
   return maxDiff
 
-def andersonDarlingTest(samples, x_min, x_max):
+def andersonDarlingTest(samples):
   import math
   samples = np.sort(samples)
-  # x_min = min(samples)
-  # x_max = max(samples)
+  x_min = min(samples)
+  x_max = max(samples)
   n = len(samples)
   aSquared_sum = 0
   epsilon = 1e-10
@@ -138,25 +199,34 @@ def secDeSeries(xs):
   plt.tight_layout()
   plt.show()
 
-def histograma(xs, mini, maxi):
-  bins = np.linspace(mini, maxi, 51)
+def histograma(xs):
+    mini = min(xs)
+    maxi = max(xs)
 
-  counts, bin_edges = np.histogram(xs, bins=bins)
+    bins = np.linspace(mini, maxi, 101)
 
-  relative_freq = counts / len(xs)
+    counts, bin_edges = np.histogram(xs, bins=bins)
+    relative_freq = counts / len(xs)
 
-  bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
+    bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
 
-  plt.figure(figsize=(10, 4))
-  plt.bar(bin_centers, relative_freq, width=bin_edges[1]-bin_edges[0],
-          color='cornflowerblue', edgecolor='black')
-  plt.axhline(y=0.02, color='red', linestyle='--', linewidth=1.5, label='Frecuencia esperada')
-  plt.title("Histograma de Frecuencia Relativa (por bin)")
-  plt.xlabel("Intervalo")
-  plt.ylabel("Frecuencia relativa")
-  plt.grid(axis='y', linestyle='--', alpha=0.7)
-  plt.tight_layout()
-  plt.show()
+    media = 0
+    desviacion = 1
+    # x = np.linspace(media - 4*desviacion, media + 4*desviacion, 1000)
+    # y = norm.pdf(x, media, desviacion)
+
+    plt.figure(figsize=(10, 4))
+    #plt.plot(x, y, label=f'N({media}, {desviacion}²)', color='blue')
+    plt.bar(bin_centers, relative_freq, width=bin_edges[1]-bin_edges[0],
+            color='cornflowerblue', edgecolor='black', alpha=0.7, label='Datos')
+    
+    plt.title("Histograma de Frecuencia Relativa vs Distribución Normal")
+    plt.xlabel("Valor")
+    plt.ylabel("Frecuencia relativa")
+    plt.legend()
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.tight_layout()
+    plt.show()
 
 def lagPlot(xs):
   valores_escala = [int (x * 1000) for x in xs]
@@ -173,3 +243,4 @@ def lagPlot(xs):
   plt.tight_layout()
   plt.show()
 
+histograma(pascal_gen(5, 0.2, 10000))
